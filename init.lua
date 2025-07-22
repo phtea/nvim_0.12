cab0d73bb52c59c537d383bfbdefcae919ab6f4d
Pushmenkov Sergey <cerejapushmenkov2@gmail.com>
2025-07-22 21:07:24 +0300

    ensure local modules are loaded correctly

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- nvim config for nvim 0.12+ (hyped for release 😃)
+package.path = vim.fn.stdpath("config") .. "/?.lua;" .. package.path -- Enable local Lua modules
 
 require("core.mappings")
 require("core.options")

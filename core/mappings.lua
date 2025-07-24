8567f0fd4130c291defd306c33e1c15c5d4abc5b
spushmenkov <spushmenkov@phoenixit.ru>
2025-07-24 20:46:58 +0300

    added mapping for tmux sessionizer

--- core/mappings.lua
+++ core/mappings.lua
@@ -13,3 +13,19 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>t", function()
+    local script = "tmux neww ~/.tmux/tmux-sessionizer"
+    local ok, output = pcall(vim.fn.system, script)
+
+    if not ok then
+        vim.notify("Ошибка вызова tmux-sessionizer: " .. output, vim.log.levels.ERROR)
+        return
+    end
+
+    if vim.v.shell_error ~= 0 then
+        vim.notify("tmux-sessionizer завершился с ошибкой:\n" .. output, vim.log.levels.WARN)
+    else
+        vim.notify("tmux-sessionizer выполнен успешно", vim.log.levels.INFO)
+    end
+end, { desc = "Запуск tmux-sessionizer", silent = true })
